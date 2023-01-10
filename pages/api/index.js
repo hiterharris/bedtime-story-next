1004d9e71d9f6775a7b2f65929bb4594b21ec349
Hiter Harris <hiterharris@comcast.net>
2023-01-09 20:12:50 -0500

    set up repo

---
+++ pages/api/index.js
@@ -0,0 +1,50 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function (req, res) {
+  if (!configuration.apiKey) {
+    
+    res.status(500).json({
+      error: {
+        message: "OpenAI API key not configured, please follow instructions in README.md",
+      }
+    });
+    return;
+  }
+
+  const prompt = req.body.prompt || '';
+  if (prompt.trim().length === 0) {
+    res.status(400).json({
+      error: {
+        message: "Please enter a valid prompt",
+      }
+    });
+    return;
+  }
+
+  try {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      max_tokens: 1000,
+      prompt: `Tell me a story about ${prompt}`,
+      temperature: 0.6,
+    });
+    res.status(200).json({ result: completion.data.choices[0].text });
+  } catch(error) {
+    if (error.response) {
+      console.error(error.response.status, error.response.data);
+      res.status(error.response.status).json(error.response.data);
+    } else {
+      console.error(`Error with OpenAI API request: ${error.message}`);
+      res.status(500).json({
+        error: {
+          message: 'An error occurred during your request.',
+        }
+      });
+    }
+  }
+}
---
+++ pages/index.js
@@ -0,0 +1,60 @@
+import Head from "next/head";
+import { useState } from "react";
+import styles from "./index.module.css";
+
+export default function Home() {
+  const [storyPrompt, setStoryPrompt] = useState("");
+  const [result, setResult] = useState();
+  const [loading, setLoading] = useState(false);
+
+  async function onSubmit(event) {
+    setLoading(true)
+    event.preventDefault();
+    try {
+      const response = await fetch("/api", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ prompt: storyPrompt }),
+      });
+
+      const data = await response.json();
+      if (response.status !== 200) {
+        throw data.error || new Error(`Request failed with status ${response.status}`);
+      }
+
+      setResult(data.result);
+      setLoading(false)
+      setStoryPrompt("");
+    } catch(error) {
+      console.error(error);
+      alert(error.message);
+    }
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>Bedtime Story</title>
+        <link rel="icon" href="/book.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <img src="/book.png" className={styles.icon} />
+        <h3>Bedtime Story</h3>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            name="animal"
+            placeholder="Enter an story topic"
+            value={storyPrompt}
+            onChange={(e) => setStoryPrompt(e.target.value)}
+          />
+          <input type="submit" value="Generate story" />
+        </form>
+        {loading ? <div className={styles.generating}>Generating story...</div> : <div className={styles.result}>{result}</div>}
+      </main>
+    </div>
+  );
+}
